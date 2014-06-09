78e6e2b3a403ffe783bd64897490d7228bfb671c
Gempalm <soichiro.kudo@gmail.com>
2014-06-09 10:57:39 +0900

    Update routes.rb
    for redmine2.5

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect "/roadmaps", :controller => "roadmaps_main", :action => "index", :conditions => { :method => :get}
+Rails.application.routes.draw do 
+  resources :projects do
+    resources :roadmaps_main
+  end
+  resources :projects, :path => 'projects/:action', :only => [:index, :show]
 end
